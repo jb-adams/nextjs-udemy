2aa6955283cf2988fdddb796d48d08c4a441b803
Jeremy Adams <jeremy.adams@ga4gh.org>
2026-07-20 21:21:48 -0400

    stuff

--- 05-onwards-foodies-starting-project/lib/actions.js
+++ 05-onwards-foodies-starting-project/lib/actions.js
@@ -2,12 +2,13 @@
 
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
+import { revalidatePath } from "next/cache";
 
 function isInvalidText(text) {
   return !text || text.trim() === '';
 }
 
-export async function shareMeal(formData) {
+export async function shareMeal(prevState, formData) {
   const meal = {
     title: formData.get('title'),
     summary: formData.get('summary'),
@@ -26,9 +27,12 @@ export async function shareMeal(formData) {
     !meal.creator_email.includes('@') ||
     !meal.image || meal.image.size === 0
   ) {
-    throw new Error('Invalid input');
+    return {
+      message: 'Invalid input.'
+    }
   }
 
   await saveMeal(meal);
+  revalidatePath('/meals');
   redirect('/meals');
 }
